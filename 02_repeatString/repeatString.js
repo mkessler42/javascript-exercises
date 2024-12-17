dfc2546d9722f177fdb25381b00b7db86f4f8e55
mkessler42 <62533844+mkessler42@users.noreply.github.com>
2024-12-17 16:06:49 -0600

    having trouble with vscode

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function getRepeatedString(string, num) {
+    var newString = ''
+    for (let i = 0; i < num; i++) {
+        newString.concat(string);
+    }
+    return newString;
 };
 
 // Do not edit below this line
